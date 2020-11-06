c9bc00373187058465c7569e244b22e2bae851cd
takeru1205 <takeru0639418@gmail.com>
2020-11-06 14:05:29 +0900

    [add] simple network

---
+++ network.py
@@ -0,0 +1,39 @@
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+class Net(nn.Module):
+    def __init__(self, lr, input_dims, n_actions, fc1_dims=128, fc2_dims=128, name='dqn', chkpt_dir='tmp/dqn'):
+        super(Net, self).__init__()
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
+        self.name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_dqn')
+
+        self.fc1 = nn.Linear(*input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.fc3 = nn.Linear(self.fc2_dims, self.n_actions)
+
+        self.optimizer = optim.RMSprop(self.parameters(), lr=lr)
+        # self.optimizer = optim.Adam(self.parameters(), lr=0.0001)
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        self.to(self.device)
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def save_checkpoint(self):
+        torch.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        torch.load_state_dict(torch.load(self.checkpoint_file))
