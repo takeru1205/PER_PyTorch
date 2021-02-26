4924b94fa1b4e9b821df7a5ae1d203bd1863bd3f
takeru1205 <takeru0639418@gmail.com>
2021-02-26 21:09:31 +0900

    [add] prioritized buffer

--- buffer.py
+++ buffer.py
@@ -1,6 +1,8 @@
 import numpy as np
 import torch
 
+from sumtree import SumTree
+
 
 class ReplayBuffer:
     """
@@ -34,7 +36,7 @@ class ReplayBuffer:
         states = self.state_memory[batch]
         states_ = self.new_state_memory[batch]
         actions = self.action_memory[batch]
-        rewards= self.reward_memory[batch]
+        rewards = self.reward_memory[batch]
         dones = self.terminal_memory[batch]
 
         return torch.from_numpy(states), torch.from_numpy(actions), torch.from_numpy(rewards), \
@@ -42,3 +44,49 @@ class ReplayBuffer:
 
     def __len__(self):
         return min(self.mem_cntr, self.mem_size)
+
+
+class PERMemory:
+    epsilon = 0.0001
+    alpha = 0.6
+    size = 0
+
+    # SumTreeについては参考にしたブログから拝借して必要なものをつけたし
+    def __init__(self, capacity):
+        self.tree = SumTree(capacity)
+
+    # Proportional prioritizationによるpriorityの計算
+    def _getPriority(self, td_error):
+        return (td_error + self.epsilon) ** self.alpha
+
+    # 新しい経験を入れる際は、必ず一度はreplyされるようにその時点で最大のpriorityで
+    # reply開始前の場合は論文に従いpriority=1とした
+    def push(self, transition):
+        self.size += 1
+
+        priority = self.tree.max()
+        if priority <= 0:
+            priority = 1
+
+        self.tree.add(priority, transition)
+
+    # 0 ~ priorityの合計値の間でbatch sizeの分だけ乱数を生成し、
+    # それに合致するデータを取得する
+    def sample(self, size):
+        list = []
+        indexes = []
+        for rand in np.random.uniform(0, self.tree.total(), size):
+            (idx, _, data) = self.tree.get(rand)
+            list.append(data)
+            indexes.append(idx)
+
+        return (indexes, list)
+
+    # 再生した経験のpriorityを更新
+    def update(self, idx, td_error):
+        priority = self._getPriority(td_error)
+        self.tree.update(idx, priority)
+
+    def __len__(self):
+        return self.size
+
